ea084d96444dcb0b4fbb2aae27161419b825a122
jigsawye <jigsaw.ye@gmail.com>
2016-01-04 13:50:03 +0800

    add eventsource-polyfill for IE9

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ var precss = require('precss');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
+    'eventsource-polyfill',
     'webpack-hot-middleware/client',
     './src'
   ],
