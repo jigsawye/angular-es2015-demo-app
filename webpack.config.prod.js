45b6c780406f8111839a48ce30e1ffc4ed0515c6
jigsawye <jigsaw.ye@gmail.com>
2016-01-08 03:14:54 +0800

    remove unnecessarily quoted

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,7 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
